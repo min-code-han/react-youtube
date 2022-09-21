db4996aceaa1d2d8b2645cb7a3701d510f3a96d5
mina <yegrinaaa@gmail.com>
2022-09-21 15:25:51 +0900

    [Feat] class형 컴포넌트 함수형 으로 변경 해보기

--- src/components/addForm.jsx
+++ src/components/addForm.jsx
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 
-class AddForm extends Component {
-  formRef = React.createRef();
-  inputRef = React.createRef();
+const AddForm = () => {
+  const formRef = useRef();
+  const inputRef = useRef();
 
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
     const name = this.inputRef.current.value;
     name && this.props.onAdd(name);
     this.formRef.current.reset();
   };
 
-  render() {
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-form-input"
-          placeholder="please enter your habit"
-        />
-        <button className="add-form-button">ADD</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-form-input"
+        placeholder="please enter your habit"
+      />
+      <button className="add-form-button">ADD</button>
+    </form>
+  );
+};
 
 export default AddForm;
